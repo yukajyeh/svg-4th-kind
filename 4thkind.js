751d4522ee778d33aadf9882e45aa4168bb085a7
Yuka Yeh <yukaka210@gmail.com>
2021-04-12 20:58:31 +0200

    light-dark mode

--- 4thkind.js
+++ 4thkind.js
@@ -4,10 +4,25 @@ const darkModeToggle = document.querySelector('a.dark-mode-toggle')
 
 const darkModeToggleText = darkModeToggle.querySelector('span')
 
-const updateDarkMode = function() {
-    if(mqDark.matches){
+const bodyTag = document.querySelector("body")
+
+darkModeToggle.addEventListener("click", function(){
+    bodyTag.classList.toggle("dark-mode")
+
+    if (bodyTag.classList.contains("dark-mode")) {
         darkModeToggleText.innerHTML = "Light mode"
     } else {
+        darkModeToggleText.innerHTML = "Dark mode"
+    }
+
+})
+
+const updateDarkMode = function() {
+    if(mqDark.matches){
+        bodyTag.classList.add("dark-mode")
+        darkModeToggleText.innerHTML = "Light mode"
+    } else {
+        bodyTag.classList.remove("dark-mode")
         darkModeToggleText.innerHTML = "Dark mode"
     }
 }
