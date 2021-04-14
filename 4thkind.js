512b9f18d5ff8de5bcc99dd981b05675a3486aaf
Yuka Yeh <yukaka210@gmail.com>
2021-04-14 17:57:24 +0200

    dark mode toggle animation

--- 4thkind.js
+++ 4thkind.js
@@ -32,8 +32,13 @@ darkModeToggle.addEventListener("click", function(){
 
     if (bodyTag.classList.contains("dark-mode")) {
         darkModeToggleText.innerHTML = "Light mode"
+
+        gsap.to("g.toggle", { x:43 })
+
     } else {
         darkModeToggleText.innerHTML = "Dark mode"
+
+        gsap.to("g.toggle", { x:19 })
     }
 
 })
@@ -42,9 +47,12 @@ const updateDarkMode = function() {
     if(mqDark.matches){
         bodyTag.classList.add("dark-mode")
         darkModeToggleText.innerHTML = "Light mode"
+        gsap.to("g.toggle", { x:43 })
+
     } else {
         bodyTag.classList.remove("dark-mode")
         darkModeToggleText.innerHTML = "Dark mode"
+        gsap.to("g.toggle", { x:19 })
     }
 }
 
